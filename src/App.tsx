bbc8371effe6bec63d2b73674fc3cdbe9f43c25f
hanzcodereal <hanzzcodee@gmail.com>
2026-08-26 23:10:53 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import { Sidebar, BottomNav } from "@/components/NavShell";
+import MiniPlayer from "@/components/MiniPlayer";
+import Home from "@/pages/Home";
+import Search from "@/pages/Search";
+import Library from "@/pages/Library";
+import PlaylistDetail from "@/pages/PlaylistDetail";
+import NowPlaying from "@/pages/NowPlaying";
+import Settings from "@/pages/Settings";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Sidebar />
+      <main className="min-h-screen px-4 pb-28 pt-6 sm:ml-64 sm:px-8 sm:pb-24">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/playlist/:id" element={<PlaylistDetail />} />
+          <Route path="/now-playing" element={<NowPlaying />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+      <MiniPlayer />
+      <BottomNav />
+    </div>
+  );
+}
